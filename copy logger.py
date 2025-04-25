ada247aa312ae18543c18720e1d58eaeec84e7fd
simit <aslansemihbiyikli@gmail.com>
2025-04-25 14:21:54 +0200

    Add files via upload

---
+++ copy logger.py
@@ -0,0 +1,79 @@
+import                     random
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+import    string # 🐢🖌️
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+import            turtledemo
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+#sfg     dh           sjdf
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+import turtle
+#sds   dh      jfsjdfsjdf
+import        socket
+import                 socketserver
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+length = int                    (input                                   ("How long should the password be? ")) # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+#sdh   fjgjsdgsjs  dgj j   fjsdf     j
+import         time # 🔠
+# 🤖 #dfsdfsdfs 🤖 # 🤖 # 🤖sdfsdfsdfs # 🤖 # 🤖 # 🤖 # 🤖 dfsdfsfd# 🤖 # 🤖 # 🤖
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖sdfsdfsdf # 🤖 # 🤖 # 🤖 sdfsdfsdfsdf# 🤖 # 🤖 # 🤖 # 🤖sdfsdfsdfsdf # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+import                    os
+print                                                                                                    ("Generating random password ...") # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+
+num = (random.randint
+       (1, length // 2))
+letters = ''.join                  (random.choices                                                                       (string.ascii_lowercase, k=length - num)) # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+
+password  =           str        (num)    +             letters
+(print
+ # 🗂️
+ # 🤖 # 🤖fsfsdf # 🤖 # 🤖 # 🤖fsdfsdfsdf # 🤖 sdfsdfsd# 🤖fsdfsdfsd # 🤖
+ # 🤖 # 🤖 # 🤖sdfsdfsdfs # 🤖 # 🤖 dfsdfsdfs# 🤖 # 🤖 # 🤖fsfsdfsd # 🤖
+ ("Your password is:",                                                                         password))
+import                               math
+
+
+import              antigravity # ⏳
+# 🤖 # 🤖 # 🤖sdfsdfsdfsf # 🤖 # 🤖 # 🤖 # 🤖 fdsdfsdfsdfsdf# 🤖 # 🤖 # 🤖 # 🤖
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+# 🤖 sdfsdfsdfs# 🤖 # 🤖 # 🤖 # 🤖 # sdfsdfsdfs🤖 # 🤖 # 🤖 # 🤖 # 🤖
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖dfsdfsdfsdf # 🤖 # 🤖
+#
+"""import                     random
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+import    string # 🐢🖌️
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+import            turtledemo
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+#sfg     dh           sjdf
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+import turtle
+#sds   dh      jfsjdfsjdf
+import        socket
+import                 socketserver
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+length = int                    (input                                   ("How long should the password be? ")) # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+#sdh   fjgjsdgsjs  dgj j   fjsdf     j
+import         time # 🔠
+# 🤖 #dfsdfsdfs 🤖 # 🤖 # 🤖sdfsdfsdfs # 🤖 # 🤖 # 🤖 # 🤖 dfsdfsfd# 🤖 # 🤖 # 🤖
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖sdfsdfsdf # 🤖 # 🤖 # 🤖 sdfsdfsdfsdf# 🤖 # 🤖 # 🤖 # 🤖sdfsdfsdfsdf # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+import                    os
+print                                                                                                    ("Generating random password ...") # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+
+num = (random.randint
+       (1, length // 2))
+letters = ''.join                  (random.choices                                                                       (string.ascii_lowercase, k=length - num)) # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+
+password  =           str        (num)    +             letters
+(print
+ # 🗂️
+ # 🤖 # 🤖fsfsdf # 🤖 # 🤖 # 🤖fsdfsdfsdf # 🤖 sdfsdfsd# 🤖fsdfsdfsd # 🤖
+ # 🤖 # 🤖 # 🤖sdfsdfsdfs # 🤖 # 🤖 dfsdfsdfs# 🤖 # 🤖 # 🤖fsfsdfsd # 🤖
+ ("Your password is:",                                                                         password))
+import                               math
+
+
+import              antigravity # ⏳
+# 🤖 # 🤖 # 🤖sdfsdfsdfsf # 🤖 # 🤖 # 🤖 # 🤖 fdsdfsdfsdfsdf# 🤖 # 🤖 # 🤖 # 🤖
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖 # 🤖
+# 🤖 sdfsdfsdfs# 🤖 # 🤖 # 🤖 # 🤖 # sdfsdfsdfs🤖 # 🤖 # 🤖 # 🤖 # 🤖
+# 🤖 # 🤖 # 🤖 # 🤖 # 🤖dfsdfsdfsdf # 🤖 # 🤖"""
